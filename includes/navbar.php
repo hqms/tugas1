86aa2a6dfc4fbc0b0165722dde56d0c3e6bfd60c
HQM <hakim@detik.com>
2017-09-14 21:46:22 +0700

    change to VSecureApp  -->VerySecureApp :D

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 	  <a class="navbar-brand" href="index.php">
-		<span class="oi oi-lock-locked" title="icon name" aria-hidden="true"></span> SecureApp
+		<span class="oi oi-lock-locked" title="icon name" aria-hidden="true"></span> VSecureApp
 		</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
