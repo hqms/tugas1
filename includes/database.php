d877dbd8f6a8ee134ffa597e3814eaa4063595db
HQM <hakim@detik.com>
2017-09-14 19:30:12 +0700

    conflict resolved

--- includes/database.php
+++ includes/database.php
@@ -14,26 +14,5 @@ $dbh = new PDO($dsn, $username, $password, $options);
 function query($sql){
 	global $dbh;
 
-<<<<<<< HEAD
-	// print ($sql);
 	return $dbh->query($sql);
-=======
-	$args = func_get_args();
-	$sql = $args[0];
-		
-	if(count($args)>1){
-		array_shift($args);	
-		$params = $args;
-	}else{
-		$params = [];
-	}	
-	
-	$stmt = $dbh->prepare($sql);
-	$stmt->execute($params);
-
-	error_log($stmt->queryString.' '. json_encode($params));
-
-	return $stmt;
-
->>>>>>> 58aaa83... add error logger
 }
